90f7747fe712aad0910dfba6f0ae3f02968d3827
Ornash <44943070+Ornashh@users.noreply.github.com>
2021-01-08 00:44:46 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+
+	let cards = $('.card-inner');
+	let cardOuter = $('.card-outer');
+	let clicked = false;
+	let lock = false;
+	let firstCard;
+	let secondCard;
+	let counter = 0;
+
+	function randomPos() {
+		for (var i = 0; i < cardOuter.length; i++) {
+			let random = Math.floor(Math.random() * cardOuter.length);
+			$(cardOuter[i]).css('order', random);
+		}
+	}
+
+	function reset() {
+		clicked = false;
+		lock = false;
+		firstCard = null;
+		secondCard = null;
+	}
+
+	function matched() {
+		$(firstCard).off();
+		$(secondCard).off();
+		reset();
+	}
+
+	function unmatched() {
+		lock = true;
+		setTimeout(() => {
+			$(firstCard).removeClass('flip');
+			$(secondCard).removeClass('flip');
+			reset();
+		}, 700);
+	}
+
+	function check() {
+		let checkCards = $(firstCard).data('social') === $(secondCard).data('social');
+		if (checkCards) {
+			matched();
+			counter++;
+		} else {
+			unmatched();
+		}
+	}
+
+	for (var i = 0; i < cards.length; i++) {
+		randomPos();
+		$(cards[i]).on('click', function() {
+			if (lock) return;
+			if (this === firstCard) return;
+
+			$(this).addClass('flip');
+
+			if (!clicked) {
+				clicked = true;
+				firstCard = this;
+				return;
+			}
+			secondCard = this;
+			check();
+
+		});
+	}
+
+});
